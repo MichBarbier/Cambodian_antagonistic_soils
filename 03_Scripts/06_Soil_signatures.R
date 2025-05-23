7326ecbe9aec4352c7801be0251b586927b47fc0
Michel Barbier <BarbierMichl@gmail.com>
2025-05-23 14:13:23 +0200

    Create 06_Soil_signatures.R

---
+++ 03_Scripts/06_Soil_signatures.R
@@ -0,0 +1,14 @@
+#########
+# This script allows to plot a Principal Component Analysis of the sampled soils 
+#########
+
+
+setwd("C:/[[YOUR PATH]]/Cambodian_antagonistic_soils")
+
+
+#### Packages ##############################
+
+library(ggsignif)
+library(
+library(tidyverse)
+
