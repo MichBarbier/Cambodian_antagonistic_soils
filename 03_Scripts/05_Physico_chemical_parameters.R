7fde1791afacad88264046cecbd12cb5074e8289
Michel Barbier <BarbierMichl@gmail.com>
2025-05-23 14:05:25 +0200

    Update and rename Physico_chemical_parameters.R to 05_Physico_chemical_parameters.R

--- 03_Scripts/05_Physico_chemical_parameters.R
+++ 03_Scripts/05_Physico_chemical_parameters.R
@@ -36,14 +36,17 @@ Var2 <- paste0("PC2 - ", round(PCA_var["Proportion Explained", "PC2"]*100, 2), "
 
 # Vector containing the shapes for each location
 Shape <- c(15,16,17)
+Colors <- c("orangered", "lightskyblue")
 
-# Plot
+## Plot
+# Due to multiple overlaps, this figure was modified with Microsoft PowerPoint
 p1 <- ggplot(PCA_scores_sites, aes(PC1,PC2))+
   geom_hline(yintercept = 0, color = "black", alpha = 0.7, size = 0.75, linetype = "dotdash")+
   geom_vline(xintercept = 0, color = "black", alpha = 0.7, size = 0.75, linetype = "dotdash")+
   geom_segment(data = PCA_scores_species, aes(x = 0, xend = PC1, y = 0, yend = PC2), color = "grey", arrow = arrow(length = unit(0.25, "cm")), alpha = 0.5)+
   geom_text_repel(data = PCA_scores_species, aes(label = Var, x = PC1, y = PC2), color = "grey", fontface = "bold")+
-  geom_point(aes(color = Data_pca$Antagonism, shape = Data_pca$Location), size = 3, color = "black")+
+  geom_point(aes(color = Data_pca$Antagonism, shape = Data_pca$Location), size = 3)+
+  scale_color_manual("Antagonism", values = Colors, guide = "none")+
   scale_shape_manual("Location", values = Shape, guide = "none")+
   geom_text_repel(aes(label = rownames(Data_pca)), fontface = "bold", nudge_x = 0.1, nudge_y = 0.09, size = 3)+
   xlab(Var1)+
@@ -51,7 +54,8 @@ p1 <- ggplot(PCA_scores_sites, aes(PC1,PC2))+
   theme_bw()+
   theme(axis.title.x = element_text(size = 11), axis.title.y = element_text(size = 11), axis.text.x = element_text(size = 10), axis.text.y = element_text(size = 10))
 
-ggsave(plot = p1, dpi = 1000, device = "svg", width = 8, height = 4, filename = "Cambodian_antagonistic_soils/04_Results/Principal_component_analysis.svg")
+
+ggsave(plot = p1, dpi = 1000, device = "svg", width = 8, height = 4, filename = "04_Results/Figure_2.svg")
 # This figure was modified with Microsoft PowerPoint to remove multiple overlaps 
 
 ## These results are presented in the Figure_1
